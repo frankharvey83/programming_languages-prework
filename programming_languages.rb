f304d92cded3a31827902fc75644cb8bf785d4bb
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-05 22:29:38 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+new_hash = {}
+
+  languages.each do |style, language|
+    language.each do |language, hash|
+    binding.pry
+      hash << style
+
+# add style to existing hash as Array
+# add that hash to new hash
+    end
+  end
 end
