060bf0f8fb0136ce5348ec3dd43f626c21d983fe
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-05 22:49:48 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -46,10 +46,10 @@ new_hash = {}
 
 end
 
-{:ruby=>{:type=>"interpreted", :style=>[:oo]},
- :javascript=>{:type=>"interpreted", :style=>[:functional]},
- :python=>{:type=>"interpreted", :style=>[:oo]},
- :java=>{:type=>"compiled", :style=>[:oo]},
- :clojure=>{:type=>"compiled", :style=>[:functional]},
- :erlang=>{:type=>"compiled", :style=>[:functional]},
- :scala=>{:type=>"compiled", :style=>[:functional]}}
+# {:ruby=>{:type=>"interpreted", :style=>[:oo]},
+#  :javascript=>{:type=>"interpreted", :style=>[:functional]},
+#  :python=>{:type=>"interpreted", :style=>[:oo]},
+#  :java=>{:type=>"compiled", :style=>[:oo]},
+#  :clojure=>{:type=>"compiled", :style=>[:functional]},
+#  :erlang=>{:type=>"compiled", :style=>[:functional]},
+#  :scala=>{:type=>"compiled", :style=>[:functional]}}
